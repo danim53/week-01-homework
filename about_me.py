6c0ec3a2c45f58b22d14f6b7d0a4e2dedef761ae
Danielle <danim53@outlook.com>
2026-05-19 20:01:51 -0700

    create program listing 3 facts

---
+++ about_me.py
@@ -0,0 +1,15 @@
+"""This program about_me.py prints 3 facts about myself
+it lists my name, favorite food, and programming goal"""
+def main() -> None:
+
+    #create and set variables
+    name = "Danielle"
+    favorite_food = "Burrito"
+
+    #print statements
+    print(f"My name is {name}")
+    print(f"My favorite food is {favorite_food}")
+    print("My programming goal for this course is to improve my creativity with python")
+
+if __name__ == '__main__':
+    main()
